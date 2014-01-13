e3fdbd959879abbb04bf8ed4e9c01178d1b92366
Jan Zegan <jzegan@gmail.com>
2014-01-13 12:04:28 +0100

    install dplyr and plotly with prereqs

---
+++ code/r/scripts/install_extras.R
@@ -0,0 +1,18 @@
+# Install Rtools before running this
+install.packages("devtools")
+library("devtools")
+install.packages("microbenchmark")
+install.packages("Lahman")
+install.packages('hflights')
+install.packages('RSQLite')
+install.packages('RSQLite.extfuns')
+devtools::install_github("dplyr")
+vignette("introduction", package = "dplyr")
+
+devtools::install_github("plotly/R-api")
+
+# library(dplyr)
+# library(data.table)
+# library(ggplot2)
+# library(reshape2)
+# library(plotly)
